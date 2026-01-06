3d4ecae3b8a56fb6486c1e0882319304fcc7bd1f
Inertia4104 <linkstreamssometimes@gmail.com>
2026-01-06 08:08:00 -0500

    First iem

--- src/main/java/net/inertia4104/bubblegumMod/BubblegumMod.java
+++ src/main/java/net/inertia4104/bubblegumMod/BubblegumMod.java
@@ -1,5 +1,6 @@
 package net.inertia4104.bubblegumMod;
 
+import net.inertia4104.bubblegumMod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -37,9 +38,8 @@ public class BubblegumMod {
     public static final String MODID = "inertiasbubblegummod";
 
     public static final Logger LOGGER = LogUtils.getLogger();
-
-
     // The constructor for the mod class is the first code that is run when your mod is loaded.
+
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public BubblegumMod(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
@@ -49,6 +49,7 @@ public class BubblegumMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -62,6 +63,8 @@ public class BubblegumMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+    if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+        event.accept(ModItems.REDGUM);}
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
