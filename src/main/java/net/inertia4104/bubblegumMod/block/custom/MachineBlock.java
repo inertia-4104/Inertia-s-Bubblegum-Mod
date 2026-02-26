fbe0b68b04387d0b039ba41e84d972a84c213c50
inertia_4104 <linkstreamssometimes@gmail.com>
2026-02-26 11:50:47 -0500

    Uhhh items interaction

--- src/main/java/net/inertia4104/bubblegumMod/block/custom/MachineBlock.java
+++ src/main/java/net/inertia4104/bubblegumMod/block/custom/MachineBlock.java
@@ -2,12 +2,20 @@ package net.inertia4104.bubblegumMod.block.custom;
 
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
@@ -18,6 +26,14 @@ public class MachineBlock extends HorizontalDirectionalBlock {
 
     public MachineBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    protected ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+        if (stack.is(Items.IRON_NUGGET))
+        {
+            return InteractionResult.SUCCESS.heldItemTransformedTo(new ItemStack((Items.BAKED_POTATO)));
+        }
     }
 
     @Override
