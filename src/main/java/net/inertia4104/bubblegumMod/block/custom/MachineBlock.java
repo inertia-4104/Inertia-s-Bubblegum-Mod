89a138679084e8b0a1e6778c839ac4400478518b
Inertia4104 <linkstreamssometimes@gmail.com>
2026-02-27 18:52:02 -0500

    Revert "Turning bubblegum machine into block entity"
    This reverts commit ad139420aac4b80fe63217a9b45cd462107e8188.

--- src/main/java/net/inertia4104/bubblegumMod/block/custom/MachineBlock.java
+++ src/main/java/net/inertia4104/bubblegumMod/block/custom/MachineBlock.java
@@ -15,9 +15,6 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-import net.minecraft.world.level.block.RenderShape;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.phys.BlockHitResult;
@@ -25,7 +22,7 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
-public class MachineBlock extends BaseEntityBlock {
+public class MachineBlock extends HorizontalDirectionalBlock {
     public static final MapCodec<MachineBlock> CODEC = simpleCodec(MachineBlock::new);
     private static final VoxelShape SHAPE = Block.box(3.0, 0.0, 3.0, 13.0, 16.0, 13.0);
 
@@ -63,16 +60,4 @@ public class MachineBlock extends BaseEntityBlock {
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(FACING);
     }
-    /* BLOCK ENTITY */
-    @Override
-    protected RenderShape getRenderShape(BlockState state){
-        return RenderShape.MODEL;
-    }
-
-    @Nullable
-    @Override
-    public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState){
-        return null;
-    }
-
 }
